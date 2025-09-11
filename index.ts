9c6d014994edef8dc104d897bb2914eaeda75b07
Josh Manders <josh@joshmanders.com>
2025-09-11 13:26:43 -0500

    initial commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = import.meta.env.PORT || 3000;
+
+app.get('/', (_, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
