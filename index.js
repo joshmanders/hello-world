d1afce84c0034d58580f2a951ad057e357fd2c1d
Josh Manders <josh@joshmanders.com>
2026-01-09 10:52:10 -0800

    frick bun all to HECK

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express();
-const port = import.meta.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (_, res) => {
   res.send('Hello, from Primcloud!');
