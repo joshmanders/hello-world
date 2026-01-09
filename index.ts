81adeae6056bd3520fa691498512e98bdca5c106
Josh Manders <josh@joshmanders.com>
2026-01-09 01:29:28 -0800

    Last try before bed

--- index.ts
+++ index.ts
@@ -3,7 +3,9 @@ import express from 'express';
 const app = express();
 const port = import.meta.env.PORT || 3000;
 
-app.get('/', (_, res) => res.send('Hello, from Primcloud!'));
+app.get('/', (_, res) => {
+  res.send('Hello, from Primcloud!')
+});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
